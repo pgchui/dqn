f546241514c1322e3324aab9c9cf9d3032d4636e
Minghao Zhu <zhu.1385@osu.edu>
2023-03-23 03:09:17 -0400

    add prioritized replay memory

--- dqn.py
+++ dqn.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
 from abc import ABC
@@ -215,7 +216,7 @@ class DuelingDQN(nn.Module):
 class Agent:
     def __init__(self, gamma, lr, input_dims, batch_size, n_actions, 
                  max_mem_size=100000, eps_max=1.0, eps_min=0.01, eps_dec=0.002, double_dqn=True, 
-                 dueling_dqn=True, noisy_net=True, learn_per_target_net_update=50, seed=None) -> None:
+                 dueling_dqn=True, noisy_net=True, prm=True, learn_per_target_net_update=50, seed=None) -> None:
         self.gamma = gamma
         self.epsilon = eps_max
         self.eps_max = eps_max
@@ -236,6 +237,11 @@ class Agent:
         self.learn_counter = 0
         self.seed=seed
         self.np_random = np.random.default_rng(seed=seed)
+        self.prm = prm,
+        if self.prm:
+            self.prm_alpha = 0.7
+            self.prm_offset = 1e-4
+            self.prm_beta = 0.5
         
         if self.seed is not None:
             torch.manual_seed(self.seed)
@@ -257,6 +263,8 @@ class Agent:
         self.action_memory = np.zeros(self.mem_size, dtype=np.int32)
         self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
         self.terminal_memory = np.zeros(self.mem_size, dtype=np.bool)
+        if self.prm:
+            self.priority_memory = np.zeros(self.mem_size, dtype=np.float32)
         
     def _update_target_net(self):
         self.Q_target.load_state_dict(self.Q_eval.state_dict())
@@ -268,6 +276,18 @@ class Agent:
         self.action_memory[idx] = action
         self.reward_memory[idx] = reward
         self.terminal_memory[idx] = done
+    
+        if self.prm:
+            # priority calculation
+            with torch.no_grad():
+                q_eval = self.Q_eval.forward(torch.as_tensor(state, device=self.Q_eval.device))[0][action].cpu().item()
+                if done:
+                    q_target = reward
+                else:
+                    q_target_next = self.Q_target.forward(torch.as_tensor(new_state, device=self.Q_target.device)).max(1)[0]
+                    q_target = reward + self.gamma * q_target_next.cpu().item()
+                error = abs(q_eval - q_target)
+            self.priority_memory[idx] = (error + self.prm_offset) ** self.prm_alpha
         
         self.mem_counter += 1
         
@@ -291,16 +311,24 @@ class Agent:
         self.Q_eval.optimizer.zero_grad()
         
         max_mem = min(self.mem_counter, self.mem_size)
-        batch = self.np_random.choice(max_mem, size=self.batch_size, replace=False)
+        if self.prm:
+            priority_sum = np.sum(self.priority_memory)
+            batch = self.np_random.choice(max_mem, p=self.priority_memory[:max_mem] / priority_sum, size=self.batch_size, replace=False)
+        else:
+            batch = self.np_random.choice(max_mem, size=self.batch_size, replace=False)
         
         batch_idx = np.arange(self.batch_size, dtype=np.int32)
         
-        state_batch = torch.as_tensor(self.state_memory[batch]).to(self.Q_eval.device)
-        new_state_batch = torch.as_tensor(self.new_state_memory[batch]).to(self.Q_eval.device)
-        reward_batch = torch.as_tensor(self.reward_memory[batch]).to(self.Q_eval.device)
-        terminal_batch = torch.as_tensor(self.terminal_memory[batch]).to(self.Q_eval.device)
+        state_batch = torch.as_tensor(self.state_memory[batch], device=self.Q_eval.device)
+        new_state_batch = torch.as_tensor(self.new_state_memory[batch], device=self.Q_eval.device)
+        reward_batch = torch.as_tensor(self.reward_memory[batch], device=self.Q_eval.device)
+        terminal_batch = torch.as_tensor(self.terminal_memory[batch], device=self.Q_eval.device)
         
         action_batch = self.action_memory[batch]
+        if self.prm:
+            priority_batch = self.priority_memory[batch]
+            weight_batch = (max_mem * priority_batch / priority_sum) ** (-self.prm_beta)
+            weight_batch /= weight_batch.max()  # normalize weigths
         
         q_eval = self.Q_eval.forward(state_batch)[batch_idx, action_batch]
         q_next = self.Q_target.forward(new_state_batch) if self.double_dqn else self.Q_eval.forward(new_state_batch)
@@ -308,7 +336,10 @@ class Agent:
         
         q_target = reward_batch + self.gamma * torch.max(q_next, dim=1)[0]
         
-        loss = self.Q_eval.loss(q_eval, q_target).to(self.Q_eval.device)
+        if self.prm:
+            loss = (torch.as_tensor(weight_batch, device=self.Q_eval.device) * F.huber_loss(q_eval, q_target).to(device=self.Q_eval.device)).mean()
+        else:
+            loss = self.Q_eval.loss(q_eval, q_target).to(self.Q_eval.device)
         loss.backward()
         self.loss = loss.item()
         self.Q_eval.optimizer.step()
